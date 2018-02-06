ced26da144ea667c6c38c1af7afa62d1dfbb3c8b
Brendan Schuetze <brendan.schuetze@gmail.com>
2018-02-06 19:57:03 +0000

    Updated credentials example.

--- Credentials_Example.R
+++ Credentials_Example.R
@@ -1,3 +1,6 @@
+# Enter your credentials into this function call. Once your credentials have been added, change the title of this file to "Credentials.R"
+# In order to obtain credentials, you need to register a Twitter API application.
+
 setup_twitter_oauth(consumer_key = "", 
                     consumer_secret= "",
                     access_token = "",
