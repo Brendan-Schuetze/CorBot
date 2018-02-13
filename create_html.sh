f33345855e084ce7b02953126189cf50e9b09008
Brendan Schuetze <brendan.schuetze@gmail.com>
2018-02-13 17:24:49 +0000

    Updated HTML Formatting Script

--- create_html.sh
+++ create_html.sh
@@ -1,4 +1,5 @@
-header.html > index.html
-sed -n '/Closing/!p' correlations.log | sed -n '/Opening/!p' >> index.html
-footer.html >> index.html
+touch index.html
+cat header.html > index.html
+sed -n '/Closing/!p' correlations.log | sed -n '/Opening/!p' | sed 's/$/<br>/'  >> index.html
+cat footer.html >> index.html
 mv index.html /var/www/html/index.html
